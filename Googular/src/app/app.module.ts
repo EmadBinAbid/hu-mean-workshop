775933bc6faf7d6c0c512566ce2f851e1f0d0397
Emad Bin Abid <emad.bin.abid@gmail.com>
2018-05-28 20:07:24 +0500

    Generated Home and Welcome components.

--- Googular/src/app/app.module.ts
+++ Googular/src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+
+const appRoutes: Routes = [
+  {path: 'home', component:  HomeComponent},
+  {path: 'welcome', component: WelcomeComponent}
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
