bd18314a29fe295843694c949280973f6819e5fc
Emad Bin Abid <emad.bin.abid@gmail.com>
2018-06-06 00:09:58 +0500

    API ready.

--- Node-API-Todo/index.js
+++ Node-API-Todo/index.js
@@ -1,15 +1,91 @@
 //Application dependencies
+var Todo = require('./models/todo.mongoose.js');
+
 var Joi = require('joi');
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+//Connecting to Mongoose
+mongoose.connect('mongodb://localhost/todo');
+var db = mongoose.connection;
+
 var app = express();
 
 //Middlewares
-
+app.use(express.json());
 
 /*Http Requests*/
+
+//addTodo
+app.post('/todo', (req, res) =>
+{
+    Todo.addTodo(req.body, function(err, todo)
+    {
+       if(err)
+       {
+        res.status(404).send("Error getting todos");
+        return;
+       }
+       res.json(todo);
+    });
+});
+
+//deleteTodo
+app.delete('/todo/:id', (req, res) => 
+{
+    Todo.deleteTodo(req.params.id, function(err, todo)
+    {
+        if(err)
+        {
+            res.status(404).send("Error getting todo.");
+            return;
+        }
+        res.json(todo);
+    });
+});
+
+//updateTodo
+app.put('/todo/:id', (req, res) => 
+{
+    Todo.updateTodo(req.params.id, req.body, {}, function(err, todo)
+    {
+        if(err)
+        {
+            res.status(404).send("Error getting todo.");
+            return;
+        }
+        res.json(todo);
+    });
+});
+
+//getTodos
+app.get('/todo', (req, res) => 
+{
+    Todo.getTodos(function(err, todos)
+    {
+       if(err)
+       {
+           res.status(404).send("Error getting todos");
+           return;
+       }
+       res.json(todos);
+    });
+});
+
+//getTodoById
+app.get('/todo/:id', (req, res) =>
+{
+    Todo.getTodoById(req.params.id, function(err, todo)
+    {
+        if(err)
+        {
+            res.status(404).send("Error getting todo.");
+            return;
+        }
+        res.json(todo);
+    });
+});
 
 
 //Creating a Node server
---
+++ Node-API-Todo/models/todo.mongoose.js
@@ -0,0 +1,53 @@
+//Application dependencies
+var mongoose = require('mongoose');
+
+//Todo Schema
+var todoSchema = mongoose.Schema(
+    {
+        name:
+        {
+            type: String,
+            required: true
+        },
+        description:
+        {
+            type: String,
+            required: true
+        }
+    }
+);
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+/*Functions to handle Http requests*/
+
+exports.addTodo = function(todoObject, callback)
+{
+    Todo.create(todoObject, callback);
+}
+
+exports.deleteTodo = function(id, callback)
+{
+    var query = { _id: id};
+    Todo.remove(query, callback);
+}
+
+exports.updateTodo = function(id, updatedTodo, options, callback)
+{
+    var query = { _id: id };
+    var update = {
+        name: updatedTodo.name,
+        description: updatedTodo.description
+    };
+    Todo.findOneAndUpdate(query, update, options, callback);
+}
+
+exports.getTodos = function(callback)
+{
+    Todo.find(callback);   
+}
+
+exports.getTodoById = function(id, callback)
+{
+    Todo.findById(id, callback);
+}
